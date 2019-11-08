0fb94866836954051e096642198f380deb0e0e1e
Piotr Komosa <komosa.piotr@gmail.com>
2019-11-08 11:57:39 +0100

    commit7 - excer1 + zad2(gitignore)

--- src/excer1/Person.java
+++ src/excer1/Person.java
@@ -15,7 +15,6 @@ public class Person {
         nameCheck(firstName, lastName);
         ageCheck(age);
     }
-
     public Person() {
 
     }
@@ -23,46 +22,36 @@ public class Person {
         if (age < 1)
             throw new IncorrectAgeException(age);
     }
-
      void nameCheck(String firstName, String lastName) throws NameUndefinedException {
         if (firstName.length() < 2 || firstName == null)
             throw new NameUndefinedException(firstName, lastName);
         if (lastName.length() < 2 || lastName == null)
             throw new NameUndefinedException(firstName, lastName);
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     public int getPesel() {
         return pesel;
     }
-
     public void setPesel(int pesel) {
         this.pesel = pesel;
     }
-
     @Override
 
     public String toString() {
@@ -74,16 +63,3 @@ public class Person {
                 '}';
     }
 }
-//    void IncorrectAgeException(int age) throws IncorrectAgeException {
-//        if (age < 2) {
-//            throw new IncorrectAgeException(age);
-//        }
-//    }
-//    void NameUndefinedException(String firstName, String lastName) throws NameUndefinedException {
-//        if (firstName.length() < 2 || lastName.length() < 2) {
-//            throw new NameUndefinedException(firstName, lastName);
-//        } else if (firstName == null || lastName == null) {
-//            throw new NameUndefinedException(firstName, lastName);
-//        }
-    
-    
